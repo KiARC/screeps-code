e8939ec6a50ef6baeb582e386030789d1fe48342
KiARC <katiearose@protonmail.com>
2022-08-14 12:59:49 -0400

    Make it so I can call the spawn function from the console

--- src/utils/MiscFunctions.ts
+++ src/utils/MiscFunctions.ts
@@ -1,5 +1,9 @@
-export function spawnCreepWithJob(spawn: StructureSpawn, job: string, body: BodyPartConstant[]) {
-  spawn.spawnCreep(
+export function spawnCreepWithJob(
+  spawn: StructureSpawn,
+  job: string,
+  body: BodyPartConstant[]
+): ScreepsReturnCode {
+  const ret = spawn.spawnCreep(
     body,
     job.concat("_", Memory.sequencer.toString()),
     {
@@ -11,16 +15,20 @@ export function spawnCreepWithJob(spawn: StructureSpawn, job: string, body: Body
     }
   );
   Memory.sequencer++;
+  return ret;
 }
 
+declare var global: any;
+global.spawnCreepWithJob = spawnCreepWithJob;
+
 export function findEnergy(creep: Creep) {
-    const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
-        filter: resource => resource.resourceType == RESOURCE_ENERGY
-      });
-      const closestDropEnergy = creep.pos.findClosestByRange(droppedEnergy)!;
-      if (creep.pickup(closestDropEnergy) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(closestDropEnergy, {
-          visualizePathStyle: { stroke: "#ffaa00" }
-        });
-      }
+  const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
+    filter: resource => resource.resourceType == RESOURCE_ENERGY
+  });
+  const closestDropEnergy = creep.pos.findClosestByRange(droppedEnergy)!;
+  if (creep.pickup(closestDropEnergy) == ERR_NOT_IN_RANGE) {
+    creep.moveTo(closestDropEnergy, {
+      visualizePathStyle: { stroke: "#ffaa00" }
+    });
+  }
 }
