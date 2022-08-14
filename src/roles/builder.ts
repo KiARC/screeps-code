c7244e99e886a55db3fd41132236aaf2e69f0ad0
KiARC <katiearose@protonmail.com>
2022-08-14 12:12:53 -0400

    Optimize builder to build closest target

--- src/roles/builder.ts
+++ src/roles/builder.ts
@@ -11,17 +11,16 @@ export var roleBuilder = {
     }
 
     if (creep.memory.working) {
-      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
       const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
       if (targets.length) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0], {
+        const closestTarget = creep.pos.findClosestByRange(targets)!;
+        if (creep.build(closestTarget) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(closestTarget, {
             visualizePathStyle: { stroke: "#ffffff" }
           });
         }
       }
     } else {
-      var sources = creep.room.find(FIND_SOURCES);
       const sources = creep.room.find(FIND_SOURCES);
       if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
         creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
